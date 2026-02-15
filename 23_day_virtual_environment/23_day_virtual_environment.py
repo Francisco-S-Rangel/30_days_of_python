ac5fd00e39997158621514ffa751b5b28cd119bf
Francisco de Souza Rangel <franciscosouza20092000@gmail.com>
2026-02-15 12:38:30 -0300

    day twenty three completed

---
+++ 23_day_virtual_environment/23_day_virtual_environment.py
@@ -0,0 +1,15 @@
+# Setting up Virtual Environments
+
+# Virtual environment can help us to create an isolated or separate environment. 
+# This will help us to avoid conflicts in dependencies across projects. 
+# If you write pip freeze on your terminal you will see all the installed packages on your computer. 
+# If we use virtualenv, we will access only packages which are specific for that project.
+
+# python -m pip freeze
+# python -m pip install virtualenv
+
+# After installing the virtualenv package go to your project folder and create a virtual env by writing:
+
+# python -m venv venv
+
+# This file has the aim to teach that is possible to work with virtual env in you machine. We not gonna do this now!!!... but  is possible
